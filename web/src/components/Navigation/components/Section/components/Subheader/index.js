75de08f78b38dea1c4f42e440066c2a00d9ad715
Marlon Landaverde <mlandaverde@heptio.com>
2018-10-02 16:53:47 -0400

    native map over lodash

--- web/src/components/Navigation/components/Section/components/Subheader/index.js
+++ web/src/components/Navigation/components/Section/components/Subheader/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import _ from 'lodash'
 import './styles.scss'
 
 export default function Subheader (props) {
   const {
-    item: { title: subheader, path = '/', children }
+    item: { title: subheader, path = '/', children = [] }
   } = props
   return (
     <li className='navigation--left-item'>
       <div className='navigation-subheader'>{subheader}</div>
-      {_.map(children, ({ title }) => (
+      {children.map(({ title }) => (
         <div key={title} className='navigation-subheader-link'>
           <Link to={path}>{title}</Link>
         </div>
