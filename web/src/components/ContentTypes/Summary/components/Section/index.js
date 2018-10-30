f474e2affd84a9fb2f14121170b07ef6e9ba3d73
Marlon Landaverde <mlandaverde@heptio.com>
2018-10-30 12:17:51 -0400

    organizes data types under components/

--- web/src/components/ContentTypes/Summary/components/Section/index.js
+++ web/src/components/ContentTypes/Summary/components/Section/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
-import CONFIG from './_config'
-
+import Basic from './components/DataTypes/Basic'
+import JSON from './components/DataTypes/JSON'
+import Link from './components/DataTypes/Link'
 import './styles.scss'
+
+const dataTypeMap = {
+  text: <Basic />,
+  json: <JSON />,
+  link: <Link />
+}
 
 export default function Section (props) {
   const { title, items } = props
@@ -10,7 +17,7 @@ export default function Section (props) {
       <div className='summary--component-title'>
         <h2>{title}</h2>
       </div>
-      {items.map((item, index) => React.cloneElement(CONFIG.dataMap[item.type], {
+      {items.map((item, index) => React.cloneElement(dataTypeMap[item.type], {
         key: index,
         params: item
       }))}
