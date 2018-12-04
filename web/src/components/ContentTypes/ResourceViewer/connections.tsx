184f6e2278e4e973b5158e8fe8f9394cf0503633
bryanl <bryanliles@gmail.com>
2018-12-04 14:11:25 -0500

    convert component to function

--- web/src/components/ContentTypes/ResourceViewer/connections.tsx
+++ web/src/components/ContentTypes/ResourceViewer/connections.tsx
@@ -13,93 +13,89 @@ interface Props {
   adjacencyList: { [key: string]: Edge[] };
 }
 
-class Connections extends React.Component<Props, any> {
-  render() {
-    const connections: JSX.Element[] = []
+export const Connections: React.StatelessComponent<Props> = ({offsets, adjacencyList}) => {
+  const connections: JSX.Element[] = []
 
-    for (const key in this.props.adjacencyList) {
-      if (this.props.offsets[key]) {
-        const parentRect = this.props.offsets[key]
-        if (parentRect) {
-          const list = this.props.adjacencyList[key]
+  for (const key in adjacencyList) {
+    if (offsets[key]) {
+      const parentRect = offsets[key]
+      if (parentRect) {
+        const list = adjacencyList[key]
 
-          connections.push(
-            ...list.map((edge: Edge, id: number) => {
-              const childRect = this.props.offsets[edge.node]
-              let start = { x: parentRect.left, y: parentRect.top }
-              let end = { x: childRect.left, y: childRect.top }
-              const edgeID = `${key}-${id}`
+        connections.push(
+          ...list.map((edge: Edge, id: number) => {
+            const childRect = offsets[edge.node]
+            let start = { x: parentRect.left, y: parentRect.top }
+            let end = { x: childRect.left, y: childRect.top }
+            const edgeID = `${key}-${id}`
 
-              const isUp = start.y > end.y
-              if (start.y > end.y) {
-                const temp = start
-                start = end
-                end = temp
-              }
+            const isUp = start.y > end.y
+            if (start.y > end.y) {
+              const temp = start
+              start = end
+              end = temp
+            }
 
-              start.x += childRect.width / 2
-              start.y += childRect.height
-              end.x += childRect.width / 2
+            start.x += childRect.width / 2
+            start.y += childRect.height
+            end.x += childRect.width / 2
 
-              let markerEnd = 'url(#down-arrow)'
-              let markerStart = ''
-              if (isUp) {
-                markerEnd = ''
-                markerStart = 'url(#up-arrow)'
-                start.y += linePadding
-                end.y -= linePadding
-              } else {
-                end.y -= markerHeight
-              }
+            let markerEnd = 'url(#down-arrow)'
+            let markerStart = ''
+            if (isUp) {
+              markerEnd = ''
+              markerStart = 'url(#up-arrow)'
+              start.y += linePadding
+              end.y -= linePadding
+            } else {
+              end.y -= markerHeight
+            }
 
-              return (
-                <line
-                  key={edgeID}
-                  className='connection'
-                  x1={start.x}
-                  y1={start.y}
-                  x2={end.x}
-                  y2={end.y}
-                  markerEnd={markerEnd}
-                  markerStart={markerStart}
-                />
-              )
-            }),
-          )
-        }
+            return (
+              <line
+                key={edgeID}
+                className='connection'
+                x1={start.x}
+                y1={start.y}
+                x2={end.x}
+                y2={end.y}
+                markerEnd={markerEnd}
+                markerStart={markerStart}
+              />
+            )
+          }),
+        )
       }
     }
-
-    return (
-      <svg width='0' height='0' className='svgContainer'>
-        <defs>
-          <marker
-            id='down-arrow'
-            markerWidth='10'
-            markerHeight='10'
-            refX='0'
-            refY='3'
-            orient='auto'
-            markerUnits='strokeWidth'
-          >
-            <path d='M0,0 L0,6 L9,3 z' fill='rgb(242,88,45)' />
-          </marker>
-          <marker
-            id='up-arrow'
-            markerWidth='10'
-            markerHeight='10'
-            refX='0'
-            refY='3'
-            orient='auto'
-            markerUnits='strokeWidth'
-          >
-            <path d='M0,3 L9,6 L9,0 z' fill='rgb(242,88,45)' />
-          </marker>
-        </defs>
-        {connections}
-      </svg>
-    )
   }
-}
 
-export default Connections
+  return (
+    <svg width='0' height='0' className='svgContainer'>
+      <defs>
+        <marker
+          id='down-arrow'
+          markerWidth='10'
+          markerHeight='10'
+          refX='0'
+          refY='3'
+          orient='auto'
+          markerUnits='strokeWidth'
+        >
+          <path d='M0,0 L0,6 L9,3 z' fill='rgb(242,88,45)' />
+        </marker>
+        <marker
+          id='up-arrow'
+          markerWidth='10'
+          markerHeight='10'
+          refX='0'
+          refY='3'
+          orient='auto'
+          markerUnits='strokeWidth'
+        >
+          <path d='M0,3 L9,6 L9,0 z' fill='rgb(242,88,45)' />
+        </marker>
+      </defs>
+      {connections}
+    </svg>
+  )
+}
