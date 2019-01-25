0dfa86d5a820ccf864d69d20ad19a3383ca1dc4c
bryanl <bryanliles@gmail.com>
2019-01-24 20:25:57 -0500

    358 - gather deployment configuration details
    * gather deployment configuration details
    * rename label selector source
    * rename expression selector source
    * add MatchOperator for expression selector to convert text to operator

--- internal/overview/printer/deployment.go
+++ internal/overview/printer/deployment.go
@@ -1,12 +1,12 @@
 package printer
 
 import (
-	"errors"
 	"fmt"
 
 	appsv1 "k8s.io/api/apps/v1"
 
 	"github.com/heptio/developer-dash/internal/view/component"
+	"github.com/pkg/errors"
 )
 
 // DeploymentListHandler is a printFunc that lists deployments
@@ -54,4 +54,97 @@ func DeploymentHandler(deployment *appsv1.Deployment, options Options) (componen
 	list := component.NewList("", []component.ViewComponent{grid})
 
 	return list, nil
+}
+
+// DeploymentConfiguration generates deployment configuration.
+type DeploymentConfiguration struct {
+	deployment *appsv1.Deployment
+}
+
+// NewDeploymentConfiguration creates an instance of DeploymentConfiguration.
+func NewDeploymentConfiguration(d *appsv1.Deployment) *DeploymentConfiguration {
+	return &DeploymentConfiguration{
+		deployment: d,
+	}
+}
+
+// Create creates a deployment configuration summary.
+func (dc *DeploymentConfiguration) Create() (*component.Summary, error) {
+	if dc.deployment == nil {
+		return nil, errors.New("deployment is nil")
+	}
+
+	sections := make([]component.SummarySection, 0)
+
+	strategyType := dc.deployment.Spec.Strategy.Type
+	sections = append(sections, component.SummarySection{
+		Header:  "Deployment Strategy",
+		Content: component.NewText("", string(strategyType)),
+	})
+
+	switch strategyType {
+	case appsv1.RollingUpdateDeploymentStrategyType:
+		rollingUpdate := dc.deployment.Spec.Strategy.RollingUpdate
+		if rollingUpdate == nil {
+			return nil, errors.Errorf("deployment strategy type is RollingUpdate, but configuration is nil")
+		}
+
+		rollingUpdateText := fmt.Sprintf("Max Surge %s%%, Max Unavailable %s%%",
+			rollingUpdate.MaxSurge.String(),
+			rollingUpdate.MaxUnavailable.String(),
+		)
+
+		sections = append(sections, component.SummarySection{
+			Header:  "Rolling Update Strategy",
+			Content: component.NewText("", rollingUpdateText),
+		})
+
+		if selector := dc.deployment.Spec.Selector; selector != nil {
+			var selectors []component.Selector
+
+			for _, lsr := range selector.MatchExpressions {
+				o, err := component.MatchOperator(string(lsr.Operator))
+				if err != nil {
+					return nil, err
+				}
+
+				es := component.NewExpressionSelector(lsr.Key, o, lsr.Values)
+				selectors = append(selectors, es)
+			}
+
+			for k, v := range selector.MatchLabels {
+				ls := component.NewLabelSelector(k, v)
+				selectors = append(selectors, ls)
+			}
+
+			sections = append(sections, component.SummarySection{
+				Header:  "Selectors",
+				Content: component.NewSelectors(selectors),
+			})
+		}
+
+		minReadySeconds := fmt.Sprintf("%d", dc.deployment.Spec.MinReadySeconds)
+		sections = append(sections, component.SummarySection{
+			Header:  "Min Ready Seconds",
+			Content: component.NewText("", minReadySeconds),
+		})
+
+		if rhl := dc.deployment.Spec.RevisionHistoryLimit; rhl != nil {
+			revisionHistoryLimit := fmt.Sprintf("%d", *rhl)
+			sections = append(sections, component.SummarySection{
+				Header:  "Revision History Limit",
+				Content: component.NewText("", revisionHistoryLimit),
+			})
+		}
+	}
+
+	creationTimestamp := dc.deployment.CreationTimestamp.Time
+	sections = append(sections, component.SummarySection{
+		Header:  "Age",
+		Content: component.NewTimestamp(creationTimestamp),
+	})
+
+	summary := component.NewSummary("Configuration", sections...)
+
+	return summary, nil
 }
