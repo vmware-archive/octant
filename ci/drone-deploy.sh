1e3165395936313d6f2f75226bf171182615c288
GuessWhoSamFoo <foos@vmware.com>
2019-06-24 19:59:38 -0700

    Added DroneCI yaml file

---
+++ ci/drone-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+if [ -z "$DRONE" ]; then
+    echo "this script is intended to be run only on drone" >&2;
+    exit 1
+fi
+
+if [ ! -z "$DRONE_TAG" ]; then
+	if [ "$(make version)" != "$DRONE_TAG" ]; then
+        echo "octant version does not match tagged version!" >&2
+        echo "octant version is $(make version)" >&2
+        echo "tag is $DRONE_TAG" >&2
+        exit 1
+    fi
+
+    goreleaser --skip-publish --rm-dist
+fi
