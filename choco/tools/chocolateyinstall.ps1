6e7f2a65b6dd73ca820af4e20343d96340ba1e48
Sam Foo <sfoohei@gmail.com>
2019-07-22 21:32:18 -0700

    Add choco nuspec and powershell install script
    Signed-off-by: Sam Foo <sfoohei@gmail.com>

---
+++ choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$packageName = 'octant'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64 = 'https://github.com/vmware/octant/releases/download/v0.5.1/octant_0.5.1_Windows-64bit.zip'
+$checksum64 = '7d2029f2388ebf141e9252a62dae10de32a4cd4dfbdad2ee345c04d5a14f3c5c'
+$checksumType64= 'sha256'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url64bit      = $url64
+
+  softwareName  = 'octant*'
+
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
