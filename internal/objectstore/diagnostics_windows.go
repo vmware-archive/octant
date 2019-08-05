e2d7a676f0c514f5df516e10834e46f0e081a600
bryanl <bryanliles@gmail.com>
2019-08-05 14:58:10 -0400

    disable dynamic cache stats in windows

---
+++ internal/objectstore/diagnostics_windows.go
@@ -0,0 +1,11 @@
+// +build windows
+
+package objectstore
+
+import (
+	"github.com/vmware/octant/internal/log"
+)
+
+func initStatusCheck(stopCh <-chan struct{}, logger log.Logger, factories *factoriesCache) {
+
+}
