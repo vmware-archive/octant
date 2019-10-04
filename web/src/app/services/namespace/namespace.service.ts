1987653049f57ec6cb56ef729913e163ef3bf3e5
GuessWhoSamFoo <foos@vmware.com>
2019-10-04 14:28:49 -0700

    Added notifier when user adds non-existent namespace
    Signed-off-by: GuessWhoSamFoo <foos@vmware.com>

--- web/src/app/services/namespace/namespace.service.ts
+++ web/src/app/services/namespace/namespace.service.ts
@@ -6,7 +6,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
-import { NotifierService, NotifierSession } from '../notifier/notifier.service';
+import {
+  NotifierService,
+  NotifierSession,
+  NotifierSignalType,
+} from '../notifier/notifier.service';
 import { WebsocketService } from '../../modules/overview/services/websocket/websocket.service';
 import { take } from 'rxjs/operators';
 
@@ -22,28 +26,49 @@ export interface UpdateNamespacesMessage {
   providedIn: 'root',
 })
 export class NamespaceService {
+  private notifierSession: NotifierSession;
+
   activeNamespace = new BehaviorSubject<string>('');
   availableNamespaces = new BehaviorSubject<string[]>([]);
 
   constructor(
     private router: Router,
     private http: HttpClient,
-    private websocketService: WebsocketService
+    private websocketService: WebsocketService,
+    private notifierService: NotifierService
   ) {
     websocketService.registerHandler('namespaces', data => {
       const update = data as UpdateNamespacesMessage;
       this.availableNamespaces.next(update.namespaces);
+
+      this.validateNamespace(update.namespaces);
     });
 
     this.activeNamespace.subscribe(namespace => {
       websocketService.sendMessage('setNamespace', { namespace });
     });
+
+    this.notifierSession = this.notifierService.createSession();
   }
 
   setNamespace(namespace: string) {
     this.activeNamespace.pipe(take(1)).subscribe(cur => {
       if (cur !== namespace) {
         this.activeNamespace.next(namespace);
+        this.validateNamespace(this.availableNamespaces.getValue());
+      }
+    });
+  }
+
+  validateNamespace(namespaces: string[]) {
+    this.activeNamespace.pipe(take(1)).subscribe(cur => {
+      if (!namespaces.includes(cur)) {
+        this.notifierSession.pushSignal(
+          NotifierSignalType.WARNING,
+          'Namespace does not exist.'
+        );
+      } else {
+        this.notifierSession.removeAllSignals();
       }
     });
   }
