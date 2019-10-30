092f0c363dd94f73e0912f1d90dd7a71a9bd5fc5
Nicolas Farruggia <nfarruggia@vmware.com>
2019-10-30 09:16:02 -0300

    TerminalComponent created using xtermjs and TabsComponent modified so the tabs can be closed
    Signed-off-by: Nicolas Farruggia <nfarruggia@vmware.com>

---
+++ web/src/app/components/terminal/terminal.component.spec.ts
@@ -0,0 +1,28 @@
+// Copyright (c) 2019 the Octant contributors. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TerminalComponent } from './terminal.component';
+import { NgTerminalModule } from 'ng-terminal';
+
+describe('TerminalComponent', () => {
+  let component: TerminalComponent;
+  let fixture: ComponentFixture<TerminalComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [NgTerminalModule],
+      declarations: [TerminalComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TerminalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
