446264db229ea2d3aa66315b56db137692a8bc56
Wayne Witzel III <wayne@riotousliving.com>
2019-11-01 15:28:50 -0400

    update choco to 0.9.1
    Signed-off-by: Wayne Witzel III <wayne@riotousliving.com>

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $ErrorActionPreference = 'Stop';
 $packageName = 'octant'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/vmware-tanzu/octant/releases/download/v0.9.0/octant_0.9.0_Windows-64bit.zip'
-$checksum64 = '2a40b89def57a6251ce92bac57d3d9d965a6b453cd4b93b98cec6ee4d7f4b31e'
+$url64 = 'https://github.com/vmware-tanzu/octant/releases/download/v0.9.1/octant_0.9.1_Windows-64bit.zip'
+$checksum64 = '3d6131a5d5e68f8e7b3977b8b614d97daa3251f20280c486f0e124eb1e5cf458'
 $checksumType64= 'sha256'
 
 $packageArgs = @{
