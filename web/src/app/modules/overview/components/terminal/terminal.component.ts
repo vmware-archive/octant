140dc704a9e6f47f4b53d3f15dbb8298410b057a
Wayne Witzel III <wayne@riotousliving.com>
2019-11-14 17:12:52 -0500

    terminal switching on the frontend
    Signed-off-by: Wayne Witzel III <wayne@riotousliving.com>

--- web/src/app/modules/overview/components/terminal/terminal.component.ts
+++ web/src/app/modules/overview/components/terminal/terminal.component.ts
@@ -1,65 +1,153 @@
 // Copyright (c) 2019 the Octant contributors. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 //
-import { Component, ViewChild, AfterViewInit, Input } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  OnInit,
+  OnDestroy,
+  AfterViewInit,
+  Input,
+} from '@angular/core';
 import { NgTerminal } from 'ng-terminal';
 import {
   TerminalOutputStreamer,
   TerminalOutputService,
 } from 'src/app/services/terminals/terminals.service';
-import { BehaviorSubject } from 'rxjs';
-import { TerminalView } from 'src/app/models/content';
+import { TerminalView, TerminalDetail } from 'src/app/models/content';
+import { WebsocketService } from '../../services/websocket/websocket.service';
 
 @Component({
   selector: 'app-terminal',
   templateUrl: './terminal.component.html',
 })
-export class TerminalComponent implements AfterViewInit {
+export class TerminalComponent implements OnInit, OnDestroy, AfterViewInit {
   private terminalStream: TerminalOutputStreamer;
+  private selectedTerminal: TerminalDetail;
   @Input() view: TerminalView;
   @ViewChild('terminal', { static: true }) child: NgTerminal;
-  scrollback: BehaviorSubject<string>;
 
-  constructor(private terminalService: TerminalOutputService) {}
+  constructor(
+    private terminalService: TerminalOutputService,
+    private wss: WebsocketService
+  ) {}
+
+  compareFn(c1: TerminalDetail, c2: TerminalDetail): boolean {
+    return c1 && c2 ? c1.uuid === c2.uuid : c1 === c2;
+  }
+
+  ngOnInit() {
+    this.initSelectedTerminal();
+  }
+
+  ngOnDestroy(): void {
+    if (this.terminalStream) {
+      this.terminalStream.scrollback.unsubscribe();
+      this.terminalStream.line.unsubscribe();
+      this.terminalStream = null;
+    }
+  }
 
   ngAfterViewInit() {
+    this.initSize();
     this.initStream();
+    this.enableResize();
 
+    let command = '';
     this.child.keyEventInput.subscribe(e => {
       const ev = e.domEvent;
       const printable = !ev.altKey && !ev.ctrlKey && !ev.metaKey;
 
       if (ev.keyCode === 13) {
-        this.child.write('\r\n$ ');
+        command += '\n';
+        this.wss.sendMessage('sendTerminalCommand', {
+          terminalID: this.selectedTerminal.uuid,
+          command,
+        });
+        console.log('sending command ' + command);
+        command = '';
       } else if (ev.keyCode === 8) {
         // Do not delete the prompt
-        if (this.child.underlying.buffer.cursorX > 2) {
+        if (this.child.underlying.buffer.cursorX > 78) {
           this.child.write('\b \b');
         }
       } else if (printable) {
+        command += e.key;
         this.child.write(e.key);
       }
+    });
+  }
+
+  onTerminalChange(): void {
+    if (this.terminalStream) {
+      this.terminalStream.scrollback.unsubscribe();
+      this.terminalStream.line.unsubscribe();
+      this.terminalStream = null;
+    }
+    this.child.underlying.clear();
+    this.initStream();
+  }
+
+  initSelectedTerminal() {
+    if (this.view) {
+      if (this.view.config.terminals && this.view.config.terminals.length > 0) {
+        this.selectedTerminal = this.view.config.terminals[0];
+      }
+    }
+  }
+
+  enableResize() {
+    let timeOut = null;
+    const resizeDebounce = (e: { cols: number; rows: number }) => {
+      const resize = () => {
+        this.wss.sendMessage('sendTerminalResize', {
+          terminalID: this.selectedTerminal.uuid,
+          rows: e.rows,
+          cols: e.cols,
+        });
+      };
+
+      if (timeOut != null) {
+        clearTimeout(timeOut);
+      }
+      timeOut = setTimeout(resize, 1000);
+    };
+    this.child.underlying.onResize(resizeDebounce);
+  }
+
+  initSize() {
+    this.wss.sendMessage('sendTerminalResize', {
+      terminalID: this.selectedTerminal.uuid,
+      rows: this.child.underlying.rows,
+      cols: this.child.underlying.cols,
     });
   }
 
   initStream() {
     const namespace = this.view.config.namespace;
     const name = this.view.config.name;
-    const container = this.view.config.container;
-    const uuid = this.view.config.uuid;
 
-    if (namespace && name && container && uuid) {
+    if (
+      namespace &&
+      name &&
+      this.selectedTerminal.container &&
+      this.selectedTerminal.uuid
+    ) {
       this.terminalStream = this.terminalService.createStream(
         namespace,
         name,
-        container,
-        uuid
+        this.selectedTerminal.container,
+        this.selectedTerminal.uuid
       );
       this.terminalStream.scrollback.subscribe((scrollback: string) => {
-        this.child.write(atob(scrollback));
+        if (scrollback && scrollback.length !== 0) {
+          this.child.write(atob(scrollback).replace(/\n/g, '\n\r'));
+        }
       });
       this.terminalStream.line.subscribe((line: string) => {
-        this.child.write(atob(line));
+        if (line && line.length !== 0) {
+          this.child.write(atob(line).replace(/\n/g, '\n\r'));
+        }
       });
     }
   }
