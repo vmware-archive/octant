c66b761a4fd655c684743bbf00a323f5996d8c8c
bryanl <bryanliles@gmail.com>
2020-01-29 07:26:27 -0500

    add github workflow for verifying generated code
    This change also removes the check from drone
    
    Signed-off-by: bryanl <bryanliles@gmail.com>

--- ci/check-generated-mocks.sh
+++ ci/check-generated-mocks.sh
@@ -3,11 +3,16 @@
 # This script makes sure that the checked-in mock files ("fake" packages) are
 # up-to-date.
 
+set -ex
+
 go run build.go go-install
 find pkg -type d -name fake -prune -exec rm -rf {} \;
 find internal -type d -name fake -prune -exec rm -rf {} \;
 go run build.go generate
+
+set +e
 diff=$(git status --porcelain | grep fake)
+set -e
 
 if [ ! -z "$diff" ]; then
     echo "The generated mock files are not up-to-date" >&2
