985a01e7fe13d77d15deab2fa2b1ab428a1435bf
Wayne Witzel III <wayne@riotousliving.com>
2020-02-20 11:49:32 -0500

    bundle ng-monaco-editor
    Signed-off-by: Wayne Witzel III <wayne@riotousliving.com>

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { highlightProvider } from './modules/shared/highlight';
+import { MonacoEditorModule, MonacoProviderService } from 'ng-monaco-editor';
 
 @Injectable()
 export class UnstripTrailingSlashLocation extends Location {
@@ -25,7 +26,12 @@ export class UnstripTrailingSlashLocation extends Location {
     BrowserAnimationsModule,
     HttpClientModule,
     RouterModule,
-
+    MonacoEditorModule.forRoot({
+      // Angular CLI currently does not handle assets with hashes. We manage it by manually adding
+      // version numbers to force library updates:
+      baseUrl: 'lib',
+      defaultOptions: {},
+    }),
     // routing loads last
     AppRoutingModule,
   ],
