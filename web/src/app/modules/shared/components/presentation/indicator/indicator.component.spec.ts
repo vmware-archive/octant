482c88376080c416c5e73384cb549069c84bcc6b
bryanl <bryanliles@gmail.com>
2020-02-20 08:13:04 -0500

    Add a status indicator to text component
    Add a status indiecator to text component to allow Octant
    to show if the item represented by the component is of
    an ok, warning, or error status.
    
    This is useful in conditions where double negatives are
    employed.
    
    Fixes: #715
    
    Signed-off-by: bryanl <bryanliles@gmail.com>

---
+++ web/src/app/modules/shared/components/presentation/indicator/indicator.component.spec.ts
@@ -0,0 +1,67 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import {
+  IndicatorComponent,
+  Status,
+  statusLookup,
+} from './indicator.component';
+import { Component } from '@angular/core';
+
+@Component({
+  template: '<app-indicator [status]="status"></app-indicator>',
+})
+class WrapperComponent {
+  status: number;
+}
+
+describe('IndicatorComponent', () => {
+  let component: WrapperComponent;
+  let fixture: ComponentFixture<WrapperComponent>;
+
+  let element: HTMLDivElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [WrapperComponent, IndicatorComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WrapperComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  [Status.Ok, Status.Warning, Status.Error].forEach(v => {
+    const name = statusLookup[v];
+
+    describe(`with ${name} status`, () => {
+      beforeEach(() => {
+        element = fixture.nativeElement;
+        component.status = v;
+        fixture.detectChanges();
+      });
+
+      it(`shows ${name} indicator`, () => {
+        expect(
+          element
+            .querySelector('app-indicator div.indicator')
+            .classList.contains(name)
+        ).toBeTruthy();
+      });
+    });
+  });
+
+  describe('with unknown status', () => {
+    beforeEach(() => {
+      element = fixture.nativeElement;
+      component.status = 0;
+      fixture.detectChanges();
+    });
+
+    it('does not show an indicator', () => {
+      console.log(element);
+      expect(element.querySelector('app-indicator div.indicator')).toBeNull();
+    });
+  });
+});
