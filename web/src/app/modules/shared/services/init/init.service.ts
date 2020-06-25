27ff72e5f278a8f1e288d5fd919c38799ae21803
Milan Klanjsek <mklanjsek@pivotal.io>
2020-06-25 09:01:42 -0600

    Regrouped component stories to workaround storybook doc selection issue, bumped storybook to v6.0.0-beta.36, switched to light theme, added code for all exiting components, improved source code visuals
    Signed-off-by: Milan Klanjsek <mklanjsek@pivotal.io>

--- web/src/app/modules/shared/services/init/init.service.ts
+++ web/src/app/modules/shared/services/init/init.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 import {
-  darkTheme,
+  lightTheme,
   ThemeService,
 } from '../../../sugarloaf/components/smart/theme-switch/theme-switch.service';
 import { MonacoProviderService } from 'ng-monaco-editor';
@@ -20,9 +20,9 @@ export class InitService {
   init(): Promise<any> {
     return new Promise((resolve, reject) => {
       try {
-        this.themeService.loadCSS(darkTheme.assetPath);
-        this.monacoService.changeTheme('vs-dark');
-        this.renderer.addClass(document.body, 'dark');
+        this.themeService.loadCSS(lightTheme.assetPath);
+        this.monacoService.changeTheme('vs');
+        this.renderer.addClass(document.body, 'light');
         resolve();
       } catch (e) {
         reject(e);
