38b5862d4aaaf631ea9954501ff6149717880820
GuessWhoSamFoo <foos@vmware.com>
2020-07-01 18:21:28 -0700

    Rename dashboard.proto to avoid name conflict
    Signed-off-by: GuessWhoSamFoo <foos@vmware.com>

--- pkg/plugin/api/proto/generate.sh
+++ pkg/plugin/api/proto/generate.sh
@@ -5,5 +5,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OCTANT_ROOT=${DIR}/../../../..
 
-protoc -I${OCTANT_ROOT}/vendor -I${OCTANT_ROOT} -I. --go_out=plugins=grpc:. dashboard.proto
+protoc -I${OCTANT_ROOT}/vendor -I${OCTANT_ROOT} -I. --go_out=plugins=grpc:. dashboard_api.proto
 
