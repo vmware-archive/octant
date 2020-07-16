c3b9db4832a275fdff9664765461e2ac6fa59953
Wayne Witzel III <wayne@riotousliving.com>
2020-07-16 12:07:54 -0400

    go generate plugin rice-box during build step
    Signed-off-by: Wayne Witzel III <wayne@riotousliving.com>

--- pkg/plugin/generate.go
+++ pkg/plugin/generate.go
@@ -5,7 +5,6 @@ SPDX-License-Identifier: Apache-2.0
 
 package plugin
 
-//go:generate rice embed-go
 //go:generate mockgen -destination=./fake/fakes.go -package=fake github.com/vmware-tanzu/octant/pkg/plugin Runners,ManagerStore,ClientFactory,ModuleService,Service,Broker
 //go:generate mockgen -source=dashboard/dashboard.pb.go -destination=./fake/mock_plugin_client.go -package=fake github.com/vmware-tanzu/octant/pkg/plugin/dashboard PluginClient
 //go:generate mockgen -source=../../vendor/github.com/hashicorp/go-plugin/protocol.go -destination=./fake/mock_client_protocol.go -package=fake github.com/hashicorp/go-plugin ClientProtocol
