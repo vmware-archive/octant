03056f65313158ae40422beebaed897c0c128faf
GuessWhoSamFoo <foos@vmware.com>
2020-09-17 15:52:10 -0700

    Fix pv list generation when claimRef pvc cannot be found
    Signed-off-by: GuessWhoSamFoo <foos@vmware.com>

---
+++ internal/objectstatus/persistent_volume.go
@@ -0,0 +1,52 @@
+/*
+Copyright (c) 2020 the Octant contributors. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package objectstatus
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+
+	"github.com/vmware-tanzu/octant/pkg/store"
+	"github.com/vmware-tanzu/octant/pkg/view/component"
+)
+
+func persistentVolume(ctx context.Context, object runtime.Object, o store.Store) (ObjectStatus, error) {
+	if object == nil {
+		return ObjectStatus{}, errors.Errorf("cronjob is nil")
+	}
+
+	pv := &corev1.PersistentVolume{}
+
+	if err := scheme.Scheme.Convert(object, pv, 0); err != nil {
+		return ObjectStatus{}, errors.Wrap(err, "convert object to v1 PersistentVolume")
+	}
+
+	if pv.Spec.ClaimRef != nil {
+		claim := pv.Spec.ClaimRef
+		pvc, err := o.Get(ctx, store.Key{
+			Kind:       claim.Kind,
+			Namespace:  claim.Namespace,
+			Name:       claim.Name,
+			APIVersion: claim.APIVersion,
+		})
+		if pvc == nil || err != nil {
+			claimWarning := fmt.Sprintf("PVC %s/%s cannot be found", claim.Namespace, claim.Name)
+			return ObjectStatus{
+				nodeStatus: component.NodeStatusWarning,
+				Details:    []component.Component{component.NewText(claimWarning)}}, nil
+		}
+	}
+
+	return ObjectStatus{
+		nodeStatus: component.NodeStatusOK,
+		Details:    []component.Component{component.NewText("v1 PersistentVolume is OK")},
+	}, nil
+}
