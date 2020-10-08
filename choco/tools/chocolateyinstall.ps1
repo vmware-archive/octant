6f9730ac6dc51d73c008b53b6c88e4f7acc8a0f8
Sam Foo <sfoohei@gmail.com>
2020-10-08 09:09:56 -0700

    Bump choco to 0.16.1
    Signed-off-by: Sam Foo <sfoohei@gmail.com>

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $ErrorActionPreference = 'Stop';
 $packageName = 'octant'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/vmware-tanzu/octant/releases/download/v0.16.0/octant_0.16.0_Windows-64bit.zip'
-$checksum64 = '24dcb8e7cdf5643b4eb472510cfb8d113acab04a062f069c72d74bf0c4961091'
+$url64 = 'https://github.com/vmware-tanzu/octant/releases/download/v0.16.1/octant_0.16.1_Windows-64bit.zip'
+$checksum64 = '07f276d1f1396a634881598fd8cd6532ed25325048081defccdb54512b6037f4'
 $checksumType64= 'sha256'
 
 $packageArgs = @{
