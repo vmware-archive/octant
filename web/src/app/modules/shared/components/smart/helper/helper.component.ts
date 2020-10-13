b4f8fb73c30bfb15d0fcb93f4d49f39f1958daa3
Milan Klanjsek <mklanjsek@pivotal.io>
2020-10-13 10:43:53 -0600

    Help menu additional items
    Signed-off-by: Milan Klanjsek <mklanjsek@pivotal.io>

--- web/src/app/modules/shared/components/smart/helper/helper.component.ts
+++ web/src/app/modules/shared/components/smart/helper/helper.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { HelperService } from '../../../../shared/services/helper/helper.service';
+import { TextView } from '../../../models/content';
 
 @Component({
   selector: 'app-helper',
@@ -11,8 +12,18 @@ export class HelperComponent implements OnInit, OnDestroy {
   version = '';
   commit = '';
   time = '';
+  releaseInfo: TextView = {
+    metadata: {
+      type: 'text',
+    },
+    config: {
+      value: '',
+      isMarkdown: true,
+    },
+  };
   isBuildModalOpen = false;
   isShortcutModalOpen = false;
+  isReleaseModalOpen = false;
   private buildInfoSubscription: Subscription;
 
   constructor(private helperService: HelperService) {}
@@ -27,6 +38,38 @@ export class HelperComponent implements OnInit, OnDestroy {
     this.buildInfoSubscription = this.helperService
       .buildTime()
       .subscribe(time => (this.time = time));
+  }
+
+  openIssue(): void {
+    window.open(
+      'https://github.com/vmware-tanzu/octant/issues/new/choose',
+      '_blank'
+    );
+  }
+
+  getReleaseInfo(version: string) {
+    const ver = version.substring(0, version.lastIndexOf('.'));
+    const baseUrl =
+      'https://raw.githubusercontent.com/vmware-tanzu/octant/master';
+    const url =
+      ver.length > 0
+        ? `${baseUrl}/changelogs/CHANGELOG-${ver}.md`
+        : `${baseUrl}/CHANGELOG.md`;
+
+    fetch(url)
+      .then(response => response.text())
+      .then(data => {
+        this.releaseInfo.config.value = data;
+      });
+  }
+
+  showReleases(): void {
+    this.getReleaseInfo(this.version);
+    this.isReleaseModalOpen = true;
+  }
+
+  showDocs(): void {
+    window.open('https://octant.dev/', '_blank');
   }
 
   ngOnDestroy(): void {
