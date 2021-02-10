33c5817708224e70ede0314a020a2d3b81ccff37
GuessWhoSamFoo <foos@vmware.com>
2021-02-10 12:53:56 -0800

    Bump to 0.17.0 and added changelogs
    Signed-off-by: GuessWhoSamFoo <foos@vmware.com>

--- build.go
+++ build.go
@@ -25,7 +25,7 @@ import (
 )
 
 var (
-	VERSION    = "v0.16.3"
+	VERSION    = "v0.17.0"
 	GOPATH     = os.Getenv("GOPATH")
 	GIT_COMMIT = gitCommit()
 	BUILD_TIME = time.Now().UTC().Format(time.RFC3339)
