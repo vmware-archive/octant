5528c5c68dc3b0de14883f095730b137e61d99f1
Wayne Witzel III <wayne@riotousliving.com>
2021-02-25 07:08:28 -0500

    implement non-cached store methods
    Signed-off-by: Wayne Witzel III <wayne@riotousliving.com>

--- internal/objectstore/store.go
+++ internal/objectstore/store.go
@@ -1,15 +1,25 @@
 package objectstore
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"io"
 	"sync"
 
-	"github.com/vmware-tanzu/octant/internal/cluster"
-	"github.com/vmware-tanzu/octant/pkg/store"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/tools/cache"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	kcache "k8s.io/client-go/tools/cache"
+	kretry "k8s.io/client-go/util/retry"
+	sigyaml "sigs.k8s.io/yaml"
+
+	"github.com/vmware-tanzu/octant/internal/cluster"
+	"github.com/vmware-tanzu/octant/internal/log"
+	"github.com/vmware-tanzu/octant/pkg/store"
 )
 
 // MemoryStore provides the struct for the store interface implementation.
@@ -22,7 +32,9 @@ type MemoryStore struct {
 
 	watcher *Watcher
 	cache   *ResourceCache
-	update  sync.Mutex
+
+	updateFns []store.UpdateFn
+	update    sync.Mutex
 }
 
 var _ store.Store = &MemoryStore{}
@@ -164,12 +176,17 @@ func (m *MemoryStore) Delete(ctx context.Context, key store.Key) error {
 	return nil
 }
 
-func (m *MemoryStore) Watch(ctx context.Context, key store.Key, handler cache.ResourceEventHandler) error {
+// Watch watches a resource.
+func (m *MemoryStore) Watch(ctx context.Context, key store.Key, handler kcache.ResourceEventHandler) error {
 	return nil
 }
+
+// Unwatch unwatches a resource.
 func (m *MemoryStore) Unwatch(ctx context.Context, groupVersionKinds ...schema.GroupVersionKind) error {
 	return nil
 }
+
+// UpdateClusterClient resets the store and updates the internal ClusterClient
 func (m *MemoryStore) UpdateClusterClient(ctx context.Context, client cluster.ClientInterface) error {
 	m.update.Lock()
 	defer m.update.Unlock()
@@ -181,17 +198,204 @@ func (m *MemoryStore) UpdateClusterClient(ctx context.Context, client cluster.Cl
 	m.watcher.StopAll()
 	m.watcher = NewWatcher(m.ctx, m.client, m.cache)
 
+	for _, u := range m.updateFns {
+		u(m)
+	}
+
 	return nil
 }
-func (m *MemoryStore) RegisterOnUpdate(fn store.UpdateFn) {}
+
+// RegisterOnUpdate registers callback functions that should be callend when UpdateClusterClient is invoked.
+func (m *MemoryStore) RegisterOnUpdate(fn store.UpdateFn) {
+	m.update.Lock()
+	defer m.update.Unlock()
+	m.updateFns = append(m.updateFns, fn)
+}
+
+// Update updates a resourec.
 func (m *MemoryStore) Update(ctx context.Context, key store.Key, updater func(*unstructured.Unstructured) error) error {
-	return nil
+	if updater == nil {
+		return fmt.Errorf("can't update object")
+	}
+
+	err := kretry.RetryOnConflict(kretry.DefaultRetry, func() error {
+		object, err := m.Get(ctx, key)
+		if err != nil {
+			return err
+		}
+
+		if object == nil {
+			return fmt.Errorf("object not found")
+		}
+
+		cacheKey, err := m.cacheKeyFromKey(key)
+		if err != nil {
+			return err
+		}
+
+		dynamicClient, err := m.client.DynamicClient()
+		if err != nil {
+			return err
+		}
+
+		if err := updater(object); err != nil {
+			return fmt.Errorf("unable to update object: %w", err)
+		}
+
+		client := dynamicClient.Resource(cacheKey.Resource).Namespace(object.GetNamespace())
+
+		_, err = client.Update(ctx, object, metav1.UpdateOptions{})
+		return err
+	})
+
+	return err
 }
+
+// IsLoading returns if a resource is currently loading.
 func (m *MemoryStore) IsLoading(ctx context.Context, key store.Key) bool {
 	return false
 }
+
+// Create creates a resource.
 func (m *MemoryStore) Create(ctx context.Context, object *unstructured.Unstructured) error {
-	return nil
+	key, err := store.KeyFromObject(object)
+	if err != nil {
+		return fmt.Errorf("key from object: %w", err)
+	}
+
+	dynamicClient, err := m.client.DynamicClient()
+	if err != nil {
+		return err
+	}
+
+	cacheKey, err := m.cacheKeyFromKey(key)
+	if err != nil {
+		return err
+	}
+
+	createOptions := metav1.CreateOptions{}
+
+	if key.Namespace == "" {
+		_, err := dynamicClient.Resource(cacheKey.Resource).Create(ctx, object, createOptions)
+		return err
+	}
+
+	_, err = dynamicClient.Resource(cacheKey.Resource).Namespace(key.Namespace).Create(ctx, object, createOptions)
+	return err
+}
+
+// CreateOrUpdateFromHandler encapulates the Apply YAML logic
+func CreateOrUpdateFromHandler(
+	ctx context.Context, namespace, input string,
+	get func(context.Context, store.Key) (*unstructured.Unstructured, error),
+	create func(context.Context, *unstructured.Unstructured) error,
+	clusterClient cluster.ClientInterface,
+) ([]string, error) {
+	withDoc := func(cb func(doc map[string]interface{}) error) error {
+		d := yaml.NewYAMLOrJSONDecoder(bytes.NewBufferString(input), 4096)
+		for {
+			doc := map[string]interface{}{}
+			if err := d.Decode(&doc); err != nil {
+				if err == io.EOF {
+					return nil
+				}
+				return fmt.Errorf("unable to parse yaml: %w", err)
+			}
+			if len(doc) == 0 {
+				// skip empty documents
+				continue
+			}
+			if err := cb(doc); err != nil {
+				return err
+			}
+		}
+	}
+
+	logger := log.From(ctx)
+	var results []string
+	err := withDoc(func(doc map[string]interface{}) error {
+		logger.Debugf("apply resource %#v", doc)
+
+		unstructuredObj := &unstructured.Unstructured{Object: doc}
+		key, err := store.KeyFromObject(unstructuredObj)
+		if err != nil {
+			return err
+		}
+
+		gvr, namespaced, err := clusterClient.Resource(key.GroupVersionKind().GroupKind())
+		if err != nil {
+			return fmt.Errorf("unable to discover resource: %w", err)
+		}
+		if namespaced && key.Namespace == "" {
+			unstructuredObj.SetNamespace(namespace)
+			key.Namespace = namespace
+		}
+
+		if _, err := get(ctx, key); err != nil {
+			if !kerrors.IsNotFound(err) {
+				// unexpected error
+				return fmt.Errorf("unable to get resource: %w", err)
+			}
+
+			// create object
+			err := create(ctx, &unstructured.Unstructured{Object: doc})
+			if err != nil {
+				return fmt.Errorf("unable to create resource: %w", err)
+			}
+
+			result := fmt.Sprintf("Created %s (%s) %s", key.Kind, key.APIVersion, key.Name)
+			if namespaced {
+				result = fmt.Sprintf("%s in %s", result, key.Namespace)
+			}
+			results = append(results, result)
+
+			return nil
+		}
+
+		// update object
+		unstructuredYaml, err := sigyaml.Marshal(doc)
+		if err != nil {
+			return fmt.Errorf("unable to marshal resource as yaml: %w", err)
+		}
+		client, err := clusterClient.DynamicClient()
+		if err != nil {
+			return fmt.Errorf("unable to get dynamic client: %w", err)
+		}
+
+		withForce := true
+		if namespaced {
+			_, err = client.Resource(gvr).Namespace(key.Namespace).Patch(
+				ctx,
+				key.Name,
+				types.ApplyPatchType,
+				unstructuredYaml,
+				metav1.PatchOptions{FieldManager: "octant", Force: &withForce},
+			)
+			if err != nil {
+				return fmt.Errorf("unable to patch resource: %w", err)
+			}
+		} else {
+			_, err = client.Resource(gvr).Patch(
+				ctx,
+				key.Name,
+				types.ApplyPatchType,
+				unstructuredYaml,
+				metav1.PatchOptions{FieldManager: "octant", Force: &withForce},
+			)
+			if err != nil {
+				return fmt.Errorf("unable to patch resource: %w", err)
+			}
+		}
+
+		result := fmt.Sprintf("Updated %s (%s) %s", key.Kind, key.APIVersion, key.Name)
+		if namespaced {
+			result = fmt.Sprintf("%s in %s", result, key.Namespace)
+		}
+		results = append(results, result)
+
+		return nil
+	})
+	return results, err
 }
 
 // CreateOrUpdateFromYAML creates resources in the cluster from YAML input.
@@ -199,5 +403,5 @@ func (m *MemoryStore) Create(ctx context.Context, object *unstructured.Unstructu
 // An error creating a resource halts resource creation.
 // A list of created resources is returned. You may have created resources AND a non-nil error.
 func (m *MemoryStore) CreateOrUpdateFromYAML(ctx context.Context, namespace, input string) ([]string, error) {
-	return []string{}, nil
+	return CreateOrUpdateFromHandler(ctx, namespace, input, m.Get, m.Create, m.client)
 }
