c0f744b0b344553a045f5c190c841283ef200993
Wayne Witzel III <wayne@riotousliving.com>
2021-07-31 13:39:48 -0400

    prevent plugins in windows from creating terminals
    Signed-off-by: Wayne Witzel III <wayne@riotousliving.com>

---
+++ pkg/plugin/plugincmd_windows.go
@@ -0,0 +1,19 @@
+// build +windows
+
+/*
+Copyright (c) 2019 the Octant contributors. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package plugin
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+func pluginCmd(cmd string) *exec.Cmd {
+	pluginCmd := exec.Command(cmd)
+	pluginCmd.SysProcAttr := &syscall.SysProcAttr{HideWindow: true}
+	return pluginCmd
+}
