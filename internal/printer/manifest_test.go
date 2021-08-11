8b5299d0a6b9e381ae2fab2d26a92bcd55a2c687
Milan Klanjsek <mklanjsek@pivotal.io>
2021-08-11 10:53:10 -0600

    Specify host os for image inspection
    Signed-off-by: Milan Klanjsek <mklanjsek@pivotal.io>

--- internal/printer/manifest_test.go
+++ internal/printer/manifest_test.go
@@ -2,7 +2,10 @@ package printer
 
 import (
 	"context"
+	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/stretchr/testify/require"
 
@@ -15,24 +18,37 @@ func Test_GetImageManifest(t *testing.T) {
 		input        string
 		manifestPath string
 		configPath   string
+		hostOS       string
+		error        string
 	}{
 		{
 			name:         "alpine",
 			input:        "docker://alpine:3.14.0",
 			manifestPath: "alpine_manifest.json",
 			configPath:   "alpine_config.json",
+			hostOS:       "linux",
 		},
 		{
 			name:         "nginx",
 			input:        "docker://nginx:1.16.1",
 			manifestPath: "nginx_manifest.json",
 			configPath:   "nginx_config.json",
+			hostOS:       "linux",
 		},
 		{
 			name:         "nginx without docker protocol",
 			input:        "nginx:1.16.1",
 			manifestPath: "nginx_manifest.json",
 			configPath:   "nginx_config.json",
+			hostOS:       "linux",
+		},
+		{
+			name:         "alpine windows",
+			input:        "docker://alpine:3.14.0",
+			manifestPath: "alpine_manifest.json",
+			configPath:   "alpine_config.json",
+			hostOS:       "windows",
+			error:        "error parsing manifest for for image docker://alpine:3.14.0: choosing image instance: no image found in manifest list for architecture amd64, variant \"\", OS windows",
 		},
 	}
 	mc := NewManifestConfiguration()
@@ -44,7 +60,13 @@ func Test_GetImageManifest(t *testing.T) {
 			inputConfig := testutil.LoadTestData(t, tt.configPath)
 			expectedConfig := string(inputConfig)[:len(inputConfig)-1]
 
-			manifest, config, err := mc.GetImageManifest(context.Background(), tt.input)
+			manifest, config, err := mc.GetImageManifest(context.Background(), tt.hostOS, tt.input)
+			fmt.Println("err", err)
+			if len(tt.error) > 0 {
+				assert.EqualError(t, err, tt.error)
+				return
+			}
+
 			require.NoError(t, err)
 			testutil.AssertJSONEqual(t, expected, manifest)
 			testutil.AssertJSONEqual(t, expectedConfig, config)
